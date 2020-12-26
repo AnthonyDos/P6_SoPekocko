db9e8c9c981cd463ccd9736a495e32c114743034
AnthonyDos <47761819+AnthonyDos@users.noreply.github.com>
2020-12-26 17:51:31 +0100

    incorporation de Helmet

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 //on importe ewpress
 const express = require('express');
-//const helmet = require('helmet')
-
+const helmet = require('helmet')
+const mongoSanitize = require("express-mongo-sanitize");
 const bodyParse = require('body-parser'); // transforme le corps de la requête en JSON 
 const mongoose = require('mongoose');
 const path = require('path')
@@ -10,6 +10,7 @@ const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
 //sécurité
+//app.use(mongoSanitize())
 require('dotenv').config();
 mongoose.connect(process.env.DB_TEST,
 {   useNewUrlParser: true,
@@ -21,18 +22,6 @@ mongoose.connect(process.env.DB_TEST,
 //permet de  cree une application express
 const app = express();
 
-// app.use(helmet())
-// app.get('/', (req, res) => { 
-//     res.send("This is the Demo page for"+ " setting up express server !") 
-// }); 
-  
-// app.listen(3000, (err) => { 
-//     if (err) { console.log(err); } 
-//     else { console.log('Server started " + "at http://localhost:3000')} 
-// }); 
-
-
-
 //permet d'accéder a l'api sans complication
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); //  tout le monde à le droit d'accéder à l'api *
@@ -41,7 +30,10 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(helmet())
+
 app.use(bodyParse.json());
+
 //routes
 app.use('/images', express.static(path.join(__dirname,'images'))) 
 app.use('/api/sauces', sauceRoutes);
